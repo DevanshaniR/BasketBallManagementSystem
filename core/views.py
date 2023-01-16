6ccad301c6dc30147509ca51002cc8818c3436a1
Rasanjika <devanshani.r@aeturnum.com>
2023-01-16 20:56:31 +0530

    added health end point

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from datetime import datetime
 
-# Create your views here.
+
+@api_view(['GET'])
+def health(request):
+    return JsonResponse({'success': True, 'status': "live", "timestamp": datetime.utcnow()})
