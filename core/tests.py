00450159a57431dd477d52d73560aa862f8ebf01
Rasanjika <devanshani.r@aeturnum.com>
2023-01-18 13:02:23 +0530

    updated tests with authentication

--- core/tests.py
+++ core/tests.py
@@ -1,5 +1,5 @@
+from django.contrib.auth.models import User
 from django.core.management import call_command
-from django.utils.http import urlencode
 from rest_framework.test import APITestCase
 from django.urls import reverse
 
@@ -8,14 +8,23 @@ from core.models import UserRole
 
 class TestBasketBall(APITestCase):
 
-    def test_get_score_board(self):
+    def create_test_data_and_authenticate(self):
         call_command('init_basketball_data')
+        user = User.objects.create_user(username="test_user", email="test@gmail.com", password="mydemo",
+                                        first_name="test_user", last_name="test_lastname")
+        user.save()
+        p = UserRole(user_id=user.id, role_id=3, is_logged_in=0)
+        p.save()
+        self.assertTrue(self.client.login(username='test_user', password='mydemo'))
+
+    def test_get_score_board(self):
+        self.create_test_data_and_authenticate()
         response = self.client.get(reverse('scoreboard'))
         self.assertEqual(response.json()['success'], True)
         self.assertIsInstance(response.json()['score_board'], list)
 
     def test_coach(self):
-        call_command('init_basketball_data')
+        self.create_test_data_and_authenticate()
         user_role = UserRole.objects.filter(role_id=2)
         user_role_coach = UserRole.objects.filter(role_id=1)
         player_id = user_role[0].user_id
@@ -26,7 +35,7 @@ class TestBasketBall(APITestCase):
         self.assertEqual(response.json()['success'], True)
 
     def test_player_score(self):
-        call_command('init_basketball_data')
+        self.create_test_data_and_authenticate()
         user_role = UserRole.objects.filter(role_id=2)
         user_role_coach = UserRole.objects.filter(role_id=1)
         player_id = user_role[0].user_id
@@ -39,7 +48,7 @@ class TestBasketBall(APITestCase):
         self.assertEqual(response.json()['success'], True)
 
     def test_team_details(self):
-        call_command('init_basketball_data')
+        self.create_test_data_and_authenticate()
         user_role = UserRole.objects.filter(role_id=2)
         user_role_coach = UserRole.objects.filter(role_id=1)
         admin = UserRole.objects.filter(role_id=3)
@@ -57,7 +66,7 @@ class TestBasketBall(APITestCase):
         self.assertEqual(response.json()['success'], True)
 
     def test_team_details(self):
-        call_command('init_basketball_data')
+        self.create_test_data_and_authenticate()
         user_role = UserRole.objects.filter(role_id=2)
         user_role_coach = UserRole.objects.filter(role_id=1)
         admin = UserRole.objects.filter(role_id=3)
